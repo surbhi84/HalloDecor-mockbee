d501152ae70db5b70376a45a01f408986f9b5d9d
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-03-21 21:10:26 +0530

    fixed hamburger issues

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import navbar from "./navbar.module.css";
 import { Carousel } from "../carousel/Carousel";
 import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Navbar = ({ isHome }) => {
+  useEffect(() => {
+    setIsHamburgerView(true);
+  }, []);
+
+  const useOnResize = (onResizefunc) => {};
+  const [winSize, setWinSize] = useState();
+  const [isHamburgerView, setIsHamburgerView] = useState(true);
   return (
     <header className="home-head">
       <Routes>
@@ -14,68 +23,84 @@ export const Navbar = ({ isHome }) => {
           isHome ? navbar["nav-head-home"] : navbar["nav-head"]
         }`}
       >
-        <Link className={navbar["logo"]} to="/">
-          <h2 className="mg-xs">Hallo!Decor</h2>
-        </Link>
-
-        <div className="menu-btn mg-xs hamburger-icon mg-left-auto">
-          <i className="fas fa-bars"></i>
+        <div className={`${navbar["nav-logo-div"]} flex-row spc-btwn`}>
+          <Link className={navbar["logo"]} to="/">
+            <h2 className="mg-xs">Hallo!Decor</h2>
+          </Link>
+          <div className="menu-btn mg-xs hamburger-icon mg-left-auto">
+            <i
+              className="fas fa-bars"
+              onClick={() => {
+                setIsHamburgerView((prev) => !prev);
+              }}
+            ></i>
+          </div>
         </div>
 
-        <ul className="nav-links">
-          <li className="search search-box">
-            <input type="search" placeholder="Search" className="search-bar" />
-            <button className="search-btn">
-              <img
-                src="/assets/icons/search.svg"
-                alt="search icon"
-                className="search-icon"
+        <div className="navBlock">
+          <ul
+            className={`nav-links ${
+              !isHamburgerView ? "nav-links-hidden" : ""
+            }`}
+          >
+            <li className="search search-box">
+              <input
+                type="search"
+                placeholder="Search"
+                className="search-bar"
               />
-            </button>
-          </li>
+              <button className="search-btn">
+                <img
+                  src="/assets/icons/search.svg"
+                  alt="search icon"
+                  className="search-icon"
+                />
+              </button>
+            </li>
 
-          <li>
-            <Link className={`nav-link ${navbar["nav-link"]}`} to="/profile">
-              <img
-                src="/assets/icons/profile.svg"
-                alt="Profile icon"
-                className="nav-icon"
-              />
-              Profile
-            </Link>
-          </li>
+            <li>
+              <Link className={` ${navbar["nav-link"]}`} to="/profile">
+                <img
+                  src="/assets/icons/profile.svg"
+                  alt="Profile icon"
+                  className="nav-icon"
+                />
+                Profile
+              </Link>
+            </li>
 
-          <li>
-            <Link className={`nav-link ${navbar["nav-link"]}`} to="/wishlist">
-              <img
-                src="/assets/icons/heartFilled.svg"
-                alt="heart icon"
-                className="nav-icon"
-              />
-              Wishlist
-            </Link>
-          </li>
+            <li>
+              <Link className={` ${navbar["nav-link"]}`} to="/wishlist">
+                <img
+                  src="/assets/icons/heartFilled.svg"
+                  alt="heart icon"
+                  className="nav-icon"
+                />
+                Wishlist
+              </Link>
+            </li>
 
-          <li>
-            <Link className={`nav-link ${navbar["nav-link"]}`} to="/cart">
-              <img
-                src="/assets/icons/cart.svg"
-                alt="cart icon"
-                className="nav-icon"
-              />
-              Cart
-            </Link>
-          </li>
+            <li>
+              <Link className={` ${navbar["nav-link"]}`} to="/cart">
+                <img
+                  src="/assets/icons/cart.svg"
+                  alt="cart icon"
+                  className="nav-icon"
+                />
+                Cart
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              to="/pages/login/"
-              className={`text-dec-none flex-col flex-center ${navbar["logout-btn"]}`}
-            >
-              Log out
-            </Link>
-          </li>
-        </ul>
+            <li>
+              <Link
+                to="/pages/login/"
+                className={` ${navbar["nav-link"]} ${navbar["logout-btn"]}`}
+              >
+                Log out
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
