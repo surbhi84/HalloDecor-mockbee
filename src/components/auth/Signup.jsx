0fdb1ae0093c5f13635ad413c7ec7a41025c0681
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-03-28 01:36:28 +0530

    add: reducer for input values,show/hide pwd logic implemented

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,7 +1,39 @@
 import auth from "./auth.module.css";
 import { Link } from "react-router-dom";
+import { useReducer, useState } from "react";
 
 export function Signup() {
+  const [signupInfo, signupDispatch] = useReducer(signup, {
+    firstName: "",
+    lastName: "",
+    email: "",
+    pwd: "",
+    confirmPwd: "",
+    tAC: false,
+  });
+
+  function signup(state, { type, payload }) {
+    switch (type) {
+      case "FIRSTNAME":
+        return { ...state, firstName: payload };
+      case "LASTNAME":
+        return { ...state, lastName: payload };
+      case "EMAIL":
+        return { ...state, email: payload };
+      case "PWD":
+        return { ...state, pwd: payload };
+      case "CONFIRMPWD":
+        return { ...state, confirmPwd: payload };
+      case "TAC":
+        return { ...state, tAc: !state.tAc };
+    }
+  }
+
+  function postUser(firstName, lastName, email, pwd) {}
+
+  const [isPwdVisible, setIsPwdVisible] = useState(false);
+  const [isConfirmPwdVisible, setIsConfirmPwdVisible] = useState(false);
+
   return (
     <>
       <main className="flex-center">
@@ -10,25 +42,53 @@ export function Signup() {
         >
           <h3 className={auth["auth-heading"]}>Sign up</h3>
 
-          {["First Name", "Last Name", "Email id"].map((label) => (
+          {[
+            { label: "First Name", type: "FIRSTNAME" },
+            { label: "Last Name", type: "LASTNAME" },
+            { label: "Email id", type: "EMAIL" },
+          ].map(({ label, type }) => (
             <label className={`flex-col gap-sm full-width`}>
               {label}
               <input
                 type="text"
                 className={`input-line ${auth["input-line"]}  full-width`}
+                onChange={(e) => {
+                  signupDispatch({ type: type, payload: e.target.value });
+                }}
               />
             </label>
           ))}
 
-          {["Choose a password", "Confirm password"].map((label) => (
+          {[
+            {
+              label: "Choose a password",
+              type: "PWD",
+              bool: isPwdVisible,
+              setter: setIsPwdVisible,
+            },
+            {
+              label: "Confirm password",
+              type: "CONFIRMPWD",
+              bool: isConfirmPwdVisible,
+              setter: setIsConfirmPwdVisible,
+            },
+          ].map(({ label, type, bool, setter }) => (
             <label className={`flex-col gap-sm full-width`}>
               {label}
               <div className="flex-row">
                 <input
-                  type="password"
+                  type={bool ? "text" : "password"}
                   className={`input-line full-width ${auth["input-line"]} `}
+                  onChange={(e) => {
+                    signupDispatch({ type: type, payload: e.target.value });
+                  }}
                 />
-                <span className="pwd-eye">
+                <span
+                  className="pwd-eye"
+                  onClick={() => {
+                    setter((p) => !p);
+                  }}
+                >
                   <i className="fas fa-eye-slash"></i>
                 </span>
               </div>
@@ -37,10 +97,36 @@ export function Signup() {
 
           <div className="flex-row spc-btwn">
             <label>
-              <input type="checkbox" /> I accept all terms & conditions
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  signupDispatch({ type: "TAC" });
+                }}
+              />
+              I accept all terms & conditions
             </label>
           </div>
-          <button className="cart-btn full-width">Create New Account</button>
+          <button
+            className="cart-btn full-width"
+            onClick={() => {
+              console.log(
+                signupInfo.firstName,
+                signupInfo.lastName,
+                signupInfo.email,
+                signupInfo.pwd,
+                signupInfo.confirmPwd,
+                signupInfo.tAc
+              );
+              postUser(
+                signupInfo.firstName,
+                signupInfo.lastName,
+                signupInfo.email,
+                signupInfo.pwd
+              );
+            }}
+          >
+            Create New Account
+          </button>
           <div>or</div>
           <Link
             to="/login"
